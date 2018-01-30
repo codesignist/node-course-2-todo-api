1b2b364b23fc0878550eed3d29a5aed414b5f418
Yunus EŞ <yunus@yunuses.com>
2018-01-30 19:31:13 +0300

    Fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || MONGOLAB_PURPLE_URI);
+mongoose.connect(process.env.MONGOLAB_PURPLE_URI);
+//process.env.MONGODB_URI
 // mongoose.connect(process.env.MONGOLAB_PURPLE_URI);
 
 module.exports = {mongoose};
